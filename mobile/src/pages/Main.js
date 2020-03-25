b6d0509ac037b00fe518e1ee338bb12d26d4b2f4
Guilherme <guilherme3394@gmail.com>
2020-03-24 21:03:10 -0300

    Cria paginas da aplicação e configura as rotas

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Main() {
+    return(
+        <View>
+            <Text>
+                Hellaeo Omni@
+            </Text>
+        </View>
+    )
+}
+
+export default Main;
---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Profile() {
+    return(
+        <View>
+            <Text>
+                Hello Omni@
+            </Text>
+        </View>
+    )
+}
+
+export default Profile;
